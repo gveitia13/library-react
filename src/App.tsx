511170a1cb7e9f272725186d54a2fb0235dec4b9
Gilbert <gveitia@xetid.cu>
2023-11-30 11:35:15 -0500

    video 6 10

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import './App.css';
+import {Navbar} from "./layouts/NavbarAndFooter/Navbar";
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+    return (
+        <Navbar></Navbar>
+    );
 }
 
 export default App;
