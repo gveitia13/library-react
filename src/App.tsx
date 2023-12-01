059cd3cd04ac1d3de92d379a2e8955fa97858f74
Gilbert Veitia Urbay <61420914+gveitia13@users.noreply.github.com>
2023-12-01 02:22:43 -0500

    10-9

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
 import {Navbar} from "./layouts/NavbarAndFooter/Navbar";
+import {ExploreTopBooks} from "./layouts/HomePage/ExploreTopBooks";
+import {Carousel} from "./layouts/HomePage/Carousel";
 
 function App() {
-    return (
-        <Navbar></Navbar>
-    );
+  return (
+    <div>
+      <Navbar></Navbar>
+      <ExploreTopBooks></ExploreTopBooks>
+      <Carousel></Carousel>
+    </div>
+  );
 }
 
 export default App;
