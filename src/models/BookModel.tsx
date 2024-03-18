0698a1928958fad2864795d0d56af85940006154
Gilberto Veitia Urbay <gveitia@xetid.cu>
2024-03-18 11:40:36 -0400

    add archivos de los statics files 15-2

--- src/models/BookModel.tsx
+++ src/models/BookModel.tsx
@@ -4,12 +4,12 @@ class BookModel {
   author?: string
   description?: string
   copies?: string
-  copiesAvailable?: string
+  copiesAvailable?: number
   category?: string
   img?: string
 
   constructor(id: number, title: string, author: string, description: string, copies: string,
-              copiesAvailable: string, category: string, img: string) {
+              copiesAvailable: number, category: string, img: string) {
     this.id = id;
     this.title = title;
     this.author = author;
