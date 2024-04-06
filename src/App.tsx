453b9f7fa6a6a4c6a3472e641597cd8dac7b8674
Gilbert Veitia Urbay <61420914+gveitia13@users.noreply.github.com>
2024-04-06 18:00:43 -0400

    17-10

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,47 @@
 import React from "react";
 import "./App.css";
-import { Navbar } from "./layouts/NavbarAndFooter/Navbar";
-import { Footer } from "./layouts/NavbarAndFooter/Footer";
-import { HomePage } from "./layouts/HomePage/HomePage";
-import { SearchBookPage } from "./layouts/SearchBookPage/SearchBookPage";
-import { Redirect, Route, Switch } from "react-router-dom";
+import {Navbar} from "./layouts/NavbarAndFooter/Navbar";
+import {Footer} from "./layouts/NavbarAndFooter/Footer";
+import {HomePage} from "./layouts/HomePage/HomePage";
+import {SearchBookPage} from "./layouts/SearchBookPage/SearchBookPage";
+import {Redirect, Route, Switch, useHistory} from "react-router-dom";
 import {BookCheckoutPage} from "./layouts/BookCheckoutPage/BookCheckoutPage";
+import {oktaConfig} from "./lib/oktaConfig";
+import {OktaAuth, toRelativeUrl} from '@okta/okta-auth-js'
+
+const oktaAuth = new OktaAuth(oktaConfig)
 
 export const App = () => {
+
+  const customAuthHandler = () => {
+    history.push('/login')
+  }
+  const history = useHistory()
+
+  const restoreOriginalUri = async (_oktaAuth: any, originalUri:any) =>{
+    history.replace(toRelativeUrl(originalUri || '/', window.location.origin))
+  }
+
   return (
     <div className="d-flex flex-column min-vh-100">
-      <Navbar />
+      <Navbar/>
       <div className="flex-grow-1">
         <Switch>
           <Route path="/" exact>
-            <Redirect to="/home" />
+            <Redirect to="/home"/>
           </Route>
           <Route path="/home">
-            <HomePage />
+            <HomePage/>
           </Route>
           <Route path="/search">
-            <SearchBookPage />
+            <SearchBookPage/>
           </Route>
           <Route path='/checkout/:bookId'>
             <BookCheckoutPage/>
           </Route>
         </Switch>
       </div>
-      <Footer />
+      <Footer/>
     </div>
   );
 };
